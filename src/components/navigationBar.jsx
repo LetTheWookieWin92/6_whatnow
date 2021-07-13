2ea18a397f124cd8573f9d80331e1271f612325e
Geoff R <70560312+LetTheWookieWin92@users.noreply.github.com>
2021-07-13 21:54:35 +1000

    added background music option

--- src/components/navigationBar.jsx
+++ src/components/navigationBar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import IconButton from '@material-ui/core/IconButton';
 import { ArrowBack } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
