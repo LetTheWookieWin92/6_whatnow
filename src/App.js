246b6f706b1e25efe51b7c482edf7d0c906311e6
Geoff R <70560312+LetTheWookieWin92@users.noreply.github.com>
2021-07-13 20:40:13 +1000

    updated homepage

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
     <div className="App">
         <Home />
         <img src={background} className="backgroundImg" style={{marginBottom: 0}} alt="Content Missing"/>
+        <div style={{backgroundColor: "#2d112b", height: 200}}>.</div>
     </div>
   );
 }
