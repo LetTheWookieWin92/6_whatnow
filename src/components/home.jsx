02340ee4757842de3cb8a4900dc0f1292f93986e
Geoff Reid <geoff_reid@outlook.com>
2021-07-25 15:23:20 +1000

    added button for testing progress and results

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React from "react";
 
 //Theme
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme";
 
 //Components
-import NavigationBar from './navigationBar';
-import BackgroundMusic from './backgroundMusic';
- 
+import NavigationBar from "./navigationBar";
+import BackgroundMusic from "./backgroundMusic";
+import { Button } from "@material-ui/core";
+
 const Home = () => {
-    return ( 
-        <ThemeProvider theme={theme}>
-            <NavigationBar textLabel="WhatNow?"  showBack="No"/>
-            <BackgroundMusic />
+	return (
+		<ThemeProvider theme={theme}>
+			<NavigationBar textLabel="WhatNow?" showBack="No" />
+			<BackgroundMusic />
 
-            <h1>.</h1>
-            
+			<Button variant="text" style={{ alignSelf: "center" }} color="primary">
+				For testing
+			</Button>
+		</ThemeProvider>
+	);
+};
 
-        </ThemeProvider>
-     );
-}
- 
 export default Home;
